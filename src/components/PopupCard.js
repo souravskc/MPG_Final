9227024e5b22190074e697dd35d2cba62bb2d8b9
Vihar Devalla <vihardevalla@gmail.com>
2021-04-21 20:51:50 +0530

    Update PopupCard.js

--- src/components/PopupCard.js
+++ src/components/PopupCard.js
@@ -9,7 +9,7 @@ function PopupCard() {
 
     if (typeof window !== "undefined") {
 
-        /* if(window.localStorage.getItem("token")){
+        if(window.localStorage.getItem("token")){
             var token = jwt_decode(window.localStorage.getItem("token"))
             if(token.login === "False"){
                 show=true;
@@ -22,7 +22,7 @@ function PopupCard() {
         else{
             var token = {"login":"False"}
             show=true;
-        } */
+        }
 
         return (
             <div className={" w-full h-full z-10 fixed align flex items-center self-center justify-center justify-self-center bg-gray-900 bg-opacity-75 lg:-my-10" } >
@@ -34,7 +34,15 @@ function PopupCard() {
         )
     }
     else{
-        return null;
+        return (
+        <div>
+            <div className={" w-full h-full z-10 fixed align flex items-center self-center justify-center justify-self-center bg-gray-900 bg-opacity-75 lg:-my-10" } >
+                <div className="bg-gray-800 h-auto m-auto p-3 -mt-1/3 border-3 block rounded-lg">
+                    {/* <span className={"text-gray-500 float-right text-3xl font-bold hover:text-black cursor-pointer -mt-2"} onClick={()=>{setShow(!show);console.log(show)}}>&times;</span> */}
+                    {/* <LoginCard /> */}
+                </div>
+            </div>
+        </div>);
     }
     
 }
