00431a174efe8618320f65f99157845ed66e6549
itachi <aggarwalaakash007@gmail.com>
2021-05-23 11:19:44 +0530

    links added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
     purge: ['./src/**/*.html', './src/**/*.js', './src/**/*.tsx'],
     darkMode: false, // or 'media' or 'class'
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'resource': '#2E77AE',
+          'content-background': '#433B3B',
+          'question': '#FCC79A'
+
+        }
+      },
     },
     variants: {
       
