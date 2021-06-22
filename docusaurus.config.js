2d9d6ddb7bf009a42326f10b01ce2c7a4e93e064
MPG2021 <84979172+MPG2021@users.noreply.github.com>
2021-06-22 22:45:40 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.png',
+  favicon: 'https://i.ibb.co/NK6yHnc/Group-218.png',
   organizationName: 'souravskc', // Usually your GitHub org/user name.
   projectName: 'MPG_Final',
 
